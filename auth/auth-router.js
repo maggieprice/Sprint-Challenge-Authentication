b3c12169253f8be20fdd72cabcbfa44b40d8a935
Justin Price <mr.price@Justins-iMac.local>
2020-02-28 12:17:38 -0500

    model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken"); 
 
-const Users = require("../users/users-model.js");
+const Users = require("./users-model.js");
+
 const { jwtSecret } = require("../config/secrets.js");
 router.post('/register', (req, res) => {
   let user = req.body;
