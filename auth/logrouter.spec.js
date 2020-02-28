28dd5317b55ba6238c891fabcdf354d120a9b756
Justin Price <mr.price@Justins-iMac.local>
2020-02-28 13:50:43 -0500

    login test passing

---
+++ auth/logrouter.spec.js
@@ -0,0 +1,13 @@
+const request = require("supertest");
+
+const server = require("../api/server.js");
+
+    describe("login", function() {
+        it('should return json', function(){
+            return request(server)
+            .post('/api/auth/login')
+            .send({ username: 'maggie', password: 'test'})
+            .expect(200)
+            .expect('Content-Type', /json/);
+        });
+  });
